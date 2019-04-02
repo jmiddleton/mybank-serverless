cc09b43fdd4f2af7afd2dbca299bf47e5f003e86
Jorge Middleton <jorge.middleton@gmail.com>
2019-04-02 12:28:21 +1100

    added banks api to retrieve registered banks

---
+++ customer/get-banks.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+const jsonResponse = require("../libs/json-response");
+
+var dynamodbOfflineOptions = {
+  region: "localhost",
+  endpoint: "http://localhost:8000"
+},
+  isOffline = () => process.env.IS_OFFLINE;
+
+const dynamoDb = isOffline()
+  ? new AWS.DynamoDB.DocumentClient(dynamodbOfflineOptions)
+  : new AWS.DynamoDB.DocumentClient();
+
+module.exports.handler = async () => {
+  const params = {
+    TableName: process.env.BANKS_TABLE
+  };
+
+  try {
+    let data = await dynamoDb.scan(params).promise();
+    return jsonResponse.ok(data.Items);
+  } catch (error) {
+    console.log(error);
+    return jsonResponse.notFound({
+      error: "Couldn\'t find banks",
+      message: error.message
+    });
+  }
+};
